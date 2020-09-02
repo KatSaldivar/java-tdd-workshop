d1246214b9438b1fdc28540c5694b81c4e8e8778
Katherine Saldivar <katherinekaufmann1@gmail.com>
2020-09-02 13:55:26 -0500

    fizz buzz

--- src/main/java/com/xyzcorp/student/FizzBuzz.java
+++ src/main/java/com/xyzcorp/student/FizzBuzz.java
@@ -2,4 +2,17 @@ package com.xyzcorp.student;
 
 public class FizzBuzz {
 
+
+    public static String convert(int i) {
+
+        if (i % 5 == 0 && i % 3 == 0){
+            return "Fizz Buzz";
+        }else if(i % 3 == 0){
+            return "Fizz";
+        }else if(i % 5 == 0){
+            return "Buzz";
+        }else{
+            return Integer.toString(i);
+        }
+    }
 }
--- src/test/java/com/xyzcorp/student/FizzBuzzTest.java
+++ src/test/java/com/xyzcorp/student/FizzBuzzTest.java
@@ -1,5 +1,40 @@
 package com.xyzcorp.student;
 
+import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class FizzBuzzTest {
+
+   /* @Test
+    public void test1(){
+        String actual = FizzBuzz.convert(1);
+        assertThat(actual).isEqualTo("1");
+    }*/
+
+    private static Stream<Arguments> intAndStringAnswerProvider(){
+        return Stream.of(
+                Arguments.of(1, "1"),
+                Arguments.of(2, "2"),
+                Arguments.of(3, "Fizz"),
+                Arguments.of(4, "4"),
+                Arguments.of(5, "Buzz"),
+                Arguments.of(15, "Fizz Buzz"),
+                Arguments.of(19, "19"),
+                Arguments.of(30, "Fizz Buzz")
+        );
+    }
+
+    @ParameterizedTest(name="{index}: int={0}, expected={1}")
+    @MethodSource("intAndStringAnswerProvider")
+    void testWithMultiArgMethodSource(int num, String expectation) {
+        assertThat(FizzBuzz.convert(num)).isEqualTo(expectation);
+    }
+
 
 }
